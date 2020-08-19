25556a818945fcf637078d2c2c8664b5b49a2f09
Hanee Park <hpark@iportfolio.co.kr>
2020-08-19 11:19:32 +0900

    Implement [SAT-202] Indicator 식별하는 기능 구현

--- src/module/text-elements.ts
+++ src/module/text-elements.ts
@@ -22,8 +22,42 @@ export class TextElements {
         this.findSingleLines();
     }
 
-    findIndicators() {
+    findIndicators(lines: LineBlock[]): void {
+        const indicatorPattern = /^[0-9]+$|^chapter[ ]*[0-9]+$|^page[ ]*[0-9]+$/i;
+        const chapterPattern = /^chapter[ ]*[0-9]+$/i;
+        const firstBlock = lines[0];
+        const lastBlock = lines[lines.length - 1];
+        const candidates = [];
 
+        if (chapterPattern.test(firstBlock.Text)) {
+            this.#indicators.push(firstBlock);
+            const secondBlock = lines[1];
+            const thirdBlock = lines[2];
+            const chapterNameDistance = secondBlock.Geometry.BoundingBox.Top - firstBlock.Geometry.BoundingBox.Top;
+            const bodyDistance = thirdBlock.Geometry.BoundingBox.Top - secondBlock.Geometry.BoundingBox.Top;
+            const averageHeight = this.getAverageHeight(lines);
+            if (secondBlock.Geometry.BoundingBox.Height > averageHeight && chapterNameDistance < bodyDistance) {
+                this.#indicators.push(secondBlock);
+            }
+            candidates.push(lastBlock);
+        } else {
+            candidates.push(firstBlock);
+            candidates.push(lastBlock);
+        }
+
+        candidates.forEach((block) => {
+            if (indicatorPattern.test(block.Text)) {
+                this.#indicators.push(block);
+            }
+        });
+    }
+
+    getAverageHeight(lines: LineBlock[]) {
+        let height = 0;
+        lines.forEach((block: LineBlock) => {
+            height += block.Geometry.BoundingBox.Height;
+        });
+        return height / lines.length;
     }
 
     findNeglictables() {
