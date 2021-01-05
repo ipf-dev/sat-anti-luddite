6e05c37ae23aef2fadd6e0d2d07ff8f79cf97b49
Hanee Park <hpark@iportfolio.co.kr>
2021-01-05 14:57:00 +0900

    Refactor [] SentenceController 예외처리 중복코드 개선

--- src/playable-sentence/authorizer.ts
+++ src/playable-sentence/authorizer.ts
@@ -1,10 +1,12 @@
 import { APIGatewayProxyEvent } from 'aws-lambda';
 
+export class AuthorizationError extends Error {}
+
 export default class Authorizer {
     public static authorize(event: APIGatewayProxyEvent) {
         const key = event.headers['x-api-key'] || event.headers['X-API-Key'];
         if (key !== process.env.PLAYABLE_SENTENCE_API_KEY) {
-            throw new Error('Unauthorized');
+            throw new AuthorizationError('Unauthorized. Invalid api key.');
         }
     }
 }
